bd9a2475b9dbd11effa47f85f2f8086f13b0aebb
Kaan Yargıcı <51912619+KaanYargc@users.noreply.github.com>
2024-04-20 09:57:51 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'sans': ['sans'],
+      'roboto': ['Roboto', 'sans-serif'],
+    }
+  },
+  content: [
+    "./*.{php,html,js}", // Tüm PHP, HTML ve JavaScript dosyalarını tarar
+  ],
+
+}
